b5f80e21bf51c78564a2f64e5ad56db13a6c6773
Camilo Picherno <camilopicherno18@gmail.com>
2024-08-14 15:38:19 -0300

    Saludo

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+let div = document.querySelector('.contenedor-boton');
+
+    div.addEventListener('click', function() {
+        alert('Hola! Soy el div')
+    });
+});
