92ad2221589259d126d0e02fb29113f51ba64c09
Camilo Picherno <camilopicherno18@gmail.com>
2024-08-14 19:28:43 -0300

    solucion

--- eventos.js
+++ eventos.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Seleccionamos el div que contiene el botón
+    let div = document.querySelector('.contenedor-boton');
+    // Seleccionamos el botón
+    let boton = document.querySelector('button');
 
-let div = document.querySelector('.contenedor-boton');
-
+    // Manejador de eventos para el div
     div.addEventListener('click', function() {
-        alert('Hola! Soy el div')
+        alert('Hola! Soy el div');
+    });
+
+    // Manejador de eventos para el botón
+    boton.addEventListener('click', function(event) {
+        event.stopPropagation(); // Detiene la propagación del evento hacia el div
+        alert('Hola! Soy el botón');
     });
 });
